1427ca613b394dcdc8165de3302d8f7803005542
seungjjun <il1uso@naver.com>
2024-11-16 15:29:35 +0900

    init backend project

---
+++ backend/src/main/java/com/techhive/TechhiveApplication.java
@@ -0,0 +1,13 @@
+package com.techhive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechhiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechhiveApplication.class, args);
+
+    }
+}
