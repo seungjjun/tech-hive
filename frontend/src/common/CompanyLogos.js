d49a2a167a4d5df6f2057f86a1c1b7026f5e8079
seungjjun <il1uso@naver.com>
2024-12-10 08:30:34 +0900

    Add Kakao logo png

--- frontend/src/common/CompanyLogos.js
+++ frontend/src/common/CompanyLogos.js
@@ -1,16 +1,17 @@
 
 const companyLogos = {
-    Toss: '/assets/img/toss-logo.png',
-    Woowahan: '/assets/img/woowahan-logo.png',
-    Kakaopay: '/assets/img/kakaopay-logo.png',
-    Kakaobank: '/assets/img/kakaobank-logo.png',
     Naver: '/assets/img/naver-logo.png',
+    Toss: '/assets/img/toss-logo.png',
+    Kakao: '/assets/img/koko-logo.png',
     Line: '/assets/img/line-logo.png',
+    Daangn: '/assets/img/daangn-logo.png',
+    Woowahan: '/assets/img/woowahan-logo.png',
     Yogiyo: '/assets/img/yogiyo-logo.png',
     Kurly: '/assets/img/kurly-logo.png',
     Yeogi: '/assets/img/yeogi-logo.png',
     Musinsa: '/assets/img/musinsa-logo.png',
-    Daangn: '/assets/img/daangn-logo.png',
+    Kakaopay: '/assets/img/kakaopay-logo.png',
+    Kakaobank: '/assets/img/kakaobank-logo.png',
     default: '/assets/img/logo192.png', // 기본 로고
 }
 
