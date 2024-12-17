660ac3f4b347d3afd93c6c4efba257136905a70d
seungjjun <il1uso@naver.com>
2024-12-17 15:10:31 +0900

    Add lombok dependency

--- backend/src/main/java/com/techhive/service/crawler/ArticleCrawler.java
+++ backend/src/main/java/com/techhive/service/crawler/ArticleCrawler.java
@@ -6,11 +6,13 @@ import com.techhive.repository.OriginTechArticleRepository;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ArticleCrawler {
